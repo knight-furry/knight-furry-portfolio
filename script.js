7fcc51f95d4895d5fa1025b65810032946f4c61a
Katkhade Dattatraya Jankiram <67061292+knight-furry@users.noreply.github.com>
2021-09-07 20:03:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+    $('a[href*=#]').bind('click', function(e) {
+            e.preventDefault(); 
+
+            var target = $(this).attr("href"); 
+            $('html, body').stop().animate({
+                scrollTop: $(target).offset().top
+                }, 600, function() {
+                    location.hash = target; 
+            });
+
+            return false;
+    });
+});
+
+$(window).scroll(function() {
+    var scrollDistance = $(window).scrollTop();
+
+    $('.page-section').each(function(i) {
+            if ($(this).position().top <= scrollDistance) {
+                    $('.main-nav a.active').removeClass('active');
+                    $('.main-nav a').eq(i).addClass('active');
+            }
+    });
+}).scroll();
+
+
+function readmore1() {
+var x = document.getElementById("more1");
+if (x.style.display === "none") {
+    x.style.display = "flex";
+} else {
+    x.style.display = "none";
+}
+}
+
+function readmore2() {
+var x = document.getElementById("more2");
+if (x.style.display === "none") {
+    x.style.display = "flex";
+} else {
+    x.style.display = "none";
+}
+}
+
+function readmore3() {
+var x = document.getElementById("more3");
+if (x.style.display === "none") {
+    x.style.display = "flex";
+} else {
+    x.style.display = "none";
+}
+}
